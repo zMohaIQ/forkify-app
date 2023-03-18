d8a7826f173857c0999e0a581506147cac57a1ba
zMohaIQ <gbyt01142@gmail.com>
2023-03-18 23:05:07 +0200

    Inital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `8621cfcd-8532-49f2-ba87-a8cee5ef9ae8`;
+export const MODAL_CLOSE_SEC = 2.5;
