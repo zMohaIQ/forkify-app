98d5186e949d326ef15aab6c4d9ec37a236fc5af
zMohaIQ <gbyt01142@gmail.com>
2023-03-18 23:35:55 +0200

    Test

--- src/js/controller.js
+++ src/js/controller.js
@@ -128,5 +128,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log(`Hello`);
 };
 init();
